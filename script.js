0633ee3b7c71ddd8bb1748ca77d4741f727931df
Papa Ndoye <pape2006ndoye@hotmail.com>
2023-03-30 15:57:10 -0400

    commit 3

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const player = document.querySelector("#player")
 const computer = document.querySelector("#computer")
 const button = document.querySelector(".start")
 const game = document.querySelector("#game")
-const scoreCheck = document.querySelector("#scoreCheck")
+const scoreCheck = document.querySelector("#score")
 const starTimer = document.querySelector("#count")
+const incrementScore = 0
 
+// create a button that's gonna start the game after the click
 
 button.addEventListener('click', function(){
-        countdown()
+        
         button.style.visibility = 'hidden'
-        game.style.visibility = 'visible'
-        scoreCheck.style.visibility = 'visible'
         starTimer.style.visibility = 'visible'
+        countdown()
 })
 
 function countdown(){
@@ -30,9 +31,13 @@ function countdown(){
       }
       if (counter === -1) {
         starTimer.style.visibility = 'hidden'
+        game.style.visibility = 'visible'
+        scoreCheck.style.visibility = 'visible'
+        computer.classList.add ('animatecomputer')
       }
       
-    },1200);
+    },1100);
+    
   }
 
 //we want it to jump when we call the JavaScript function
@@ -51,6 +56,20 @@ function countdown(){
   }
 //  adding collisions for when we don’t dodge the block with a jump.
 // For this, we will make a function that runs every 10ms and checks if you lost or not.
-// then we fetch the top position of the character and the left position of the block.
+// then we fetch the top position of the playerand the left position of the block.
 // We set an interval running our function (every 10ms). 
 // We put that in a variable and parsed as an integer, so the parsing returns the number and not a string
+
+
+let hitCheck = setInterval (function(){
+  let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+  let computerLeft = parseInt(window.getComputedStyle(computer).getPropertyValue("left"));
+  if(computerLeft<20 && computerLeft>-20 && playerTop>=225){
+      computer.style.animation = "none";
+      alert("Game Over. You lost score: "+Math.floor(incrementScore/100 + " Refresh this page to replay."));
+      incrementScore=0;
+  }else{
+    incrementScore++;
+      document.getElementById("scoreCheck").innerHTML = Math.floor(incrementScore/100);
+  }
+},10)
